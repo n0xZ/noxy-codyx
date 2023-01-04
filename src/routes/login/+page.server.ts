22bf15fa6525789ff97b8c31cccfc6513d864605
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-01-04 17:23:05 -0300

    setup formData validation on login action

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,18 +1,29 @@
-import { PrismaClient } from '@prisma/client'
 import { fail, redirect } from '@sveltejs/kit'
+import { z } from 'zod'
+import bcryptjs from 'bcryptjs'
 import type { Actions } from './$types'
-type LoginFormData = {
-	username: string
-	password: string
-}
+import { prisma } from 'src/utils/prisma'
+
+const loginSchema = z.object({
+	email: z.string().email({ message: 'Email ingresado no valido' }),
+	password: z.string().min(5, { message: 'Campo requerido' }),
+})
 export const actions: Actions = {
 	loginUser: async ({ request }) => {
-		const formData = Object.fromEntries(await request.formData()) as LoginFormData
-		const client = new PrismaClient()
-		const existingUser = await client.user.findUnique({
-			where: { username: formData.username },
-		})
-		if (!existingUser) return fail(400, { error: 'User does not exist.' })
-		return redirect(301, '/home')
+		const formData = loginSchema.safeParse(await request.formData())
+
+		if (formData.success) {
+			const existingUser = await prisma.user.findUnique({
+				where: { email: formData.data.email },
+			})
+			if (!existingUser) return
+			const passwordMatches = await bcryptjs.compare(
+				formData.data.email,
+				existingUser.password
+			)
+			if (!passwordMatches) return
+			return redirect(301, '/home')
+		}
+		return fail(400, { formErrors: formData.error })
 	},
 }
