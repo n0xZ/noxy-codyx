be4b5cb55ad12f905269808e4ea18bece03471c3
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-01-04 17:23:18 -0300

    add home header skeleton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,28 @@
+<header class="px-8 py-6 ">
+	<nav
+		class="flex flex-row items-center justify-between container mx-auto max-w-5xl xl:text-lg text-base font-normal"
+	>
+		<h1>
+			<a href="/" class="no-underline"> Codyx </a>
+		</h1>
+		<ul class="xl:flex hidden flex-row items-center space-x-4  ">
+			<li>
+				<a href="/login" class="px-8 py-4 rounded-lg no-underline">
+					Iniciar sesión
+				</a>
+			</li>
+			<li>
+				<a
+					href="/register"
+					class="px-8 py-4 rounded-lg bg-orange-400  no-underline text-light-50"
+				>
+					Unete ya!
+				</a>
+			</li>
+		</ul>
+	</nav>
+</header>
+<main class="min-h-screen h-full flex flex-col justify-center items-center space-y-3" >
+<h1></h1>
 
-<main class="min-h-screen h-full grid place-items-center"  >
-<form method="post" action="?index">
-
-</form>
 </main>
