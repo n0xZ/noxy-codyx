41c2558b8feca304829cd1a19da0c4cc455574ec
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-01-10 16:58:50 -0300

    chore: add field errors integration

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,16 +1,42 @@
 <script lang="ts">
-import type{  ActionData } from './$types'
-export let form:ActionData
-
+	import { enhance } from '$app/forms'
+	import type { ActionData } from './$types'
+	export let form: ActionData
 </script>
 
-<main class="h-screen">
-	<form action="/login?/loginUser">
-		<aside>
-		<label for="username">Nombre de usuario</label>
-      	<input type="text" name="username" />
+<main class="h-screen grid place-items-center">
+	<form action="/login?/loginUser" use:enhance method="post" class=" flex flex-col justify-center space-y-6 max-w-2xl w-full xl:p-0 p-1">
+		<aside class="flex flex-col justify-center space-y-3 max-w-2xl">
+			<label for="email" class="font-semibold">Correo electrónico</label>
+			<input
+				type="email"
+				name="email"
+				class="px-4 py-3 rounded-lg outline-none bg-purple-50 c-gray-800 max-w-2xl"
+			/>
+			<span class="h-5 c-red-500"
+				>{#if form?.containsErrors}
+					<p>{form?.fields?.email}</p>
+				{/if}</span
+			>
 		</aside>
-		<input type="password" name="password" />
-	<button type="submit">Iniciar sesión</button>
-   </form>
+		<aside class="flex flex-col justify-center space-y-3 max-w-2xl">
+			<label for="password" class="font-semibold">Contraseña</label>
+
+			<input
+				type="password"
+				name="password"
+				class="px-4 py-3 rounded-lg outline-none bg-purple-50 c-gray-800 max-w-2xl"
+			/>
+				<span class="h-5 c-red-500"
+				>{#if form?.containsErrors}
+					<p>{form?.fields?.password}</p>
+				{/if}</span
+			>
+		</aside>
+		<button
+			type="submit"
+			class="px-5 py-3 rounded-lg outline-none bg-purple-300 c-gray-600 font-semibold w-full max-w-2xl"
+			>Iniciar sesión</button
+		>
+	</form>
 </main>
