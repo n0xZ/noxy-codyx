e63e39891f9ab004131868ce6962a50f3993ac3e
n0xZ <o.gonzalo@hotmail.com>
2023-01-10 23:25:23 -0300

    add redirect if user's not logged in on home page

--- src/routes/home/+page.server.ts
+++ src/routes/home/+page.server.ts
@@ -1,6 +1,10 @@
-import { redirect } from '@sveltejs/kit'
+import { redirect, type ServerLoad } from '@sveltejs/kit'
 import type { Actions } from './$types'
 
+export const load: ServerLoad = async ({ cookies }) => {
+	const loggedUser = cookies.get('user-session')
+	if (!loggedUser) return redirect(302, '/')
+}
 export const actions: Actions = {
 	logout: async ({ cookies }) => {
 		cookies.delete('user-session')
