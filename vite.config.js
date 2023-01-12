e1d1a9a54eb084e39ec2a92204e0e430cc4d4e7f
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-01-12 17:32:00 -0300

    chore: setup custom fonts

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import { presetWebFonts, presetUno } from 'unocss'
 import Unocss from 'unocss/vite'
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit(), Unocss()],
+	plugins: [
+		sveltekit(),
+		Unocss({
+			presets: [
+				presetUno(),
+				presetWebFonts({
+					provider: 'google',
+					fonts: {
+						commissioner: 'Commissioner',
+						mukta: 'Mukta',
+					},
+				}),
+			],
+		}),
+	],
 }
 
 export default config
