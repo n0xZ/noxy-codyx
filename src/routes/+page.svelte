e0c50896f914d94755e4b58d52e5472106d909ea
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-01-12 18:02:48 -0300

    chore: add new styles on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,23 @@
-<header class="px-8 py-6 ">
+<svelte:head>
+<title>Bienvenid@ a Codyx!</title>
+</svelte:head>
+<header class="p-6 ">
 	<nav
-		class="flex flex-row items-center justify-between container mx-auto max-w-5xl xl:text-lg text-base font-normal"
+		class="flex flex-row items-center justify-between container mx-auto max-w-5xl xl:text-lg text-base font-normal  font-commissioner "
 	>
 		<h1>
-			<a href="/" class="no-underline"> Codyx </a>
+			<a href="/" class="no-underline hover:c-pink-500 duration-100 ease-in-out font-semibold">CODYX</a>
 		</h1>
 		<ul class="xl:flex hidden flex-row items-center space-x-4  ">
 			<li>
-				<a href="/login" class="px-8 py-4 rounded-lg no-underline">
-					Iniciar sesión
+				<a href="/login" class="px-8 py-4 rounded-lg no-underline hover:c-pink-500 duration-100 ease-in-out">
+					INICIAR SESIÓN
 				</a>
 			</li>
 			<li>
 				<a
 					href="/register"
-					class="px-8 py-4 rounded-lg bg-orange-400  no-underline text-light-50"
+					class="px-8 py-4 rounded-lg border border-2 border-pink-500  no-underline text-light-50"
 				>
 					Unete ya!
 				</a>
@@ -22,7 +25,8 @@
 		</ul>
 	</nav>
 </header>
-<main class="min-h-screen h-full flex flex-col justify-center items-center space-y-3" >
-<h1></h1>
+<section class="min-h-screen h-full flex flex-col justify-center items-center space-y-5 font-mukta" >
 
-</main>
+<h2 class="text-center xl:text-5xl text-3xl p-2">Imposible olvidar tu   <span class="c-pink-500">lista de recomendaciones</span>  con Codyx</h2>
+<a href="/login" class="px-6 py-4 outline-none bg-pink-500 rounded-lg ">Comenzar ya</a>
+</section>
