19f4778474b67ae094d551a964b1eb5013e91ceb
n0xZ <o.gonzalo@hotmail.com>
2023-01-12 21:48:02 -0300

    chore: setup new styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,4 @@ import '@unocss/reset/antfu.css'
 import 'virtual:uno.css'
 </script>
 
-<main class="bg-warmgray-900 c-gray-50 h-full w-full min-h-screen"><slot/></main>
+<main class="bg-rose-100 c-gray-900 h-full w-full min-h-screen"><slot/></main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,46 @@
 <svelte:head>
-<title>Bienvenid@ a Codyx!</title>
+	<title>Bienvenid@ a Codyx!</title>
 </svelte:head>
 <header class="p-6 ">
 	<nav
 		class="flex flex-row items-center justify-between container mx-auto max-w-5xl xl:text-lg text-base font-normal  font-commissioner "
 	>
 		<h1>
-			<a href="/" class="no-underline hover:c-pink-500 duration-100 ease-in-out font-semibold">CODYX</a>
+			<a
+				href="/"
+				class="no-underline list-upper-roman hover:c-rose-500 duration-100 ease-in-out font-semibold"
+				>Codyx</a
+			>
 		</h1>
 		<ul class="xl:flex hidden flex-row items-center space-x-4  ">
 			<li>
-				<a href="/login" class="px-8 py-4 rounded-lg no-underline hover:c-pink-500 duration-100 ease-in-out">
+				<a
+					href="/login"
+					class="px-8 py-4 rounded-lg no-underline hover:c-rose-500 duration-100 ease-in-out"
+				>
 					INICIAR SESIÓN
 				</a>
 			</li>
 			<li>
 				<a
 					href="/register"
-					class="px-8 py-4 rounded-lg border border-2 border-pink-500  no-underline text-light-50"
+					class="px-8 py-4 rounded-lg no-underline text-light-50 font-commissioner bg-rose-500  "
 				>
-					Unete ya!
+					UNETE YA
 				</a>
 			</li>
 		</ul>
 	</nav>
 </header>
-<section class="min-h-screen h-full flex flex-col justify-center items-center space-y-5 font-mukta" >
-
-<h2 class="text-center xl:text-5xl text-3xl p-2">Imposible olvidar tu   <span class="c-pink-500">lista de recomendaciones</span>  con Codyx</h2>
-<a href="/login" class="px-6 py-4 outline-none bg-pink-500 rounded-lg ">Comenzar ya</a>
+<section
+	class="min-h-screen h-full flex flex-col justify-center items-center space-y-5 font-mukta"
+>
+	<h2 class="text-center xl:text-5xl text-3xl p-2">
+		Imposible olvidar tu <span class="c-rose-500 ">lista de recomendaciones</span>
+		con Codyx
+	</h2>
+	
+	<a href="/login" class="px-6 py-4 outline-none bg-rose-500 rounded-lg  c-gray-50"
+		>Comenzar ya</a
+	>
 </section>
