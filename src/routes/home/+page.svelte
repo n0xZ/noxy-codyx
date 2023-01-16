002640616492fa0e616bb59e62e02a5fad84d5db
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-01-16 17:06:29 -0300

    set new styles on home page

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
  import type { PageData, PageServerData } from "./$types"
-import {enhance} from '$app/forms'
 import RecommendationList from "$lib/components/recommendation/recommendation-list.svelte";
 
 
@@ -12,10 +11,11 @@ export let data :PageServerData
 </svelte:head>
 
 <h2 class="mt-6 text-center font-bold text-2xl font-mukta">Mi lista actual de recomendaciones</h2>
-<section class="h-full container mx-auto max-w-3xl   grid xl:grid-cols-6 grid-cols-1 grid-rows-2  place-items-center mt-4 font-mukta">
+
 {#if data.reccos.length!==0}
+<section class="h-full container mx-auto max-w-3xl   grid xl:grid-cols-2  grid-cols-1 grid-rows-2 gap-5  place-items-center mt-4 font-mukta">
 	<RecommendationList props={{reccos:data.reccos}}/>
+	</section>
 	{:else }
-	<p>Al parecer no tienes recomendados creados hasta ahora. Puedes crearlos haciendo <a href="/home/recommendations/create">click aquí</a></p>
+	<p class="text-center ">Al parecer no tienes recomendados creados hasta ahora. Puedes crearlos haciendo <a class="underline" href="/home/recommendations/create">click aquí</a></p>
 {/if}
-</section>
