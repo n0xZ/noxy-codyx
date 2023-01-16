b53f22423750f72f51877f589595b5082fdcd117
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-01-16 16:58:42 -0300

    chore: setup menu hamburger icon

---
+++ src/lib/components/icons/menu.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+export let menuClass:string;
+</script>
+<svg
+	fill="currentColor"
+	stroke-width="0"
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 16 16"
+	height="1em"
+	width="1em"
+	style="overflow: visible;"
+   class={menuClass}
+	><path
+		fill-rule="evenodd"
+		d="M14 5H2V3h12v2zm0 4H2V7h12v2zM2 13h12v-2H2v2z"
+		clip-rule="evenodd"
+	/></svg
+>
