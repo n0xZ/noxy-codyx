dab4070aaeae1f2d9439654acf48afdc110abdef
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-01-16 16:32:46 -0300

    chore: add new icons

---
+++ src/lib/components/icons/edit.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let iconClass: string
+</script>
+
+<svg
+	fill="none"
+	stroke-width="2"
+	xmlns="http://www.w3.org/2000/svg"
+	stroke="currentColor"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+   class={iconClass}
+	viewBox="0 0 24 24"
+	height="1em"
+	width="1em"
+	style="overflow: visible;"
+	><path d="M17 3a2.828 2.828 0 114 4L7.5 20.5 2 22l1.5-5.5L17 3z" /></svg
+>
