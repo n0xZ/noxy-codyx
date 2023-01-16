e0b925130d902c496afee0b916ad02c89fc1a91b
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-01-16 14:06:57 -0300

    remove content related to layout

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -2,7 +2,7 @@
  import type { PageData, PageServerData } from "./$types"
 import {enhance} from '$app/forms'
 import RecommendationList from "$lib/components/recommendation-list.svelte";
- import type { Recommendation } from "@prisma/client"
+
 
 export let data :PageServerData
 
@@ -10,21 +10,12 @@ export let data :PageServerData
 <svelte:head>
 <title>Codyx - Home</title>
 </svelte:head>
-<header class="p-4 border-b-2 border-rose-200">
-	<nav
-		class="flex flex-row items-center justify-between container mx-auto max-w-5xl xl:text-lg text-base font-normal font-commissioner "
-	>
-		<h1>
-			<a href="/home" class="no-underline c-rose-500  font-semibold"> Codyx </a>
-		</h1>
-		<ul class="xl:flex hidden flex-row items-center space-x-4  ">
-			<form method="post" use:enhance  action="/home?/logout">
-				<button type="submit" class="p-3 rounded-lg bg-rose-500 c-gray-50 font-semibold ">Cerrar sesión</button>
-			</form>
-		</ul>
-	</nav>
-</header>
+
 <h2 class="mt-6 text-center font-bold text-2xl font-mukta">Mi lista actual de recomendaciones</h2>
-<section class="h-full  container mx-auto grid grid-cols-6 grid-rows-2s  mt-4 font-mukta">
- <RecommendationList props={{reccos:data.reccos}}/>
+<section class="h-full   grid xl:grid-cols-6 grid-cols-1 grid-rows-2  mt-4 font-mukta">
+{#if data.reccos.length!==0}
+	<RecommendationList props={{reccos:data.reccos}}/>
+	{:else }
+	<p>Al parecer no tienes recomendados creados hasta ahora. Puedes crearlos haciendo <a href="/home/recommendations/create">click aquí</a></p>
+{/if}
 </section>
