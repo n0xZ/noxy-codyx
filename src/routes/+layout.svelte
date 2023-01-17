8e05654ac08a8a945ec235871a13c827e1f77675
n0xZ <o.gonzalo@hotmail.com>
2023-01-17 20:31:36 -0300

    remove rose bg color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,4 @@ import '@unocss/reset/antfu.css'
 import 'virtual:uno.css'
 </script>
 
-<main class="bg-rose-100 c-gray-900 h-full w-full min-h-screen"><slot/></main>
+<main class=" c-gray-900 h-full w-full min-h-screen"><slot/></main>
