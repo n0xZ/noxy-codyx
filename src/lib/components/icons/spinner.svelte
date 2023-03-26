572eddeb16a347851b6bd44fdf596dafe6e21305
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-03-26 11:22:48 -0300

    chore: add spinner icon

---
+++ src/lib/components/icons/spinner.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+</script>
+
+<svg
+	fill="none"
+	stroke-width="0"
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 24 24"
+	height="1em"
+	width="1em"
+class="h-6 w-6 animate-spin"
+	><path
+		fill="currentColor"
+		fill-rule="evenodd"
+		d="M12 19a7 7 0 1 0 0-14 7 7 0 0 0 0 14Zm0 3c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10Z"
+		clip-rule="evenodd"
+		opacity=".2"
+	/><path
+		fill="currentColor"
+		d="M2 12C2 6.477 6.477 2 12 2v3a7 7 0 0 0-7 7H2Z"
+	/></svg
+>
