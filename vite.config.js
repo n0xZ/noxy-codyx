6f2625701b191af6ca30757c18c14d9e607ffa51
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-01 09:17:51 -0300

    remove unocss plugin

--- vite.config.js
+++ vite.config.js
@@ -1,44 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite'
-import { presetWebFonts, presetUno } from 'unocss'
-import Unocss from 'unocss/vite'
-import { createRequire } from 'module'
-
-const prismaPlugin = () => {
-	const require = createRequire(import.meta.url)
-	const pathName = require
-		.resolve('@prisma/client')
-		.replace('@prisma/client/index.js', '')
-
-	return {
-		name: 'prisma-vite-plugin',
-		config: () => ({
-			resolve: {
-				alias: {
-					'.prisma/client/index-browser': `${pathName}.prisma/client/index-browser.js`,
-				},
-			},
-		}),
-	}
-}
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [
-		sveltekit(),
-		prismaPlugin(),
-		Unocss({
-			presets: [
-				presetUno(),
-				presetWebFonts({
-					provider: 'google',
-					fonts: {
-						commissioner: 'Commissioner',
-						mukta: 'Mukta',
-					},
-				}),
-			],
-		}),
-	],
+	plugins: [sveltekit()],
 }
 
 export default config
