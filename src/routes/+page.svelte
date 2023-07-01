2f17820a8a3541128bfc271daeb4ecb96f0bbf77
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-01 11:37:24 -0300

    chore: add new styles on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,20 +8,20 @@ import MobileLandingMenu from '$lib/components/menu/landing.svelte'
 </svelte:head>
 <header class="p-6 ">
 	<nav
-		class="flex flex-row items-center justify-between container mx-auto max-w-5xl xl:text-lg text-base font-normal  font-commissioner "
+		class="flex flex-row items-center justify-between container mx-auto max-w-5xl xl:text-lg text-base    "
 	>
 		<h1>
 			<a
 				href="/"
-				class="no-underline list-upper-roman hover:c-rose-500 duration-100 ease-in-out font-semibold"
+				class="no-underline list-upper-roman hover:text-rose-500 duration-100 ease-in-out font-semibold"
 				>Codyx</a
 			>
 		</h1>
-		<ul class="xl:flex hidden flex-row items-center space-x-4  ">
+		<ul class="xl:flex hidden flex-row items-center space-x-4  opacity-90">
 			<li>
 				<a
 					href="/login"
-					class="px-8 py-4 rounded-lg no-underline hover:c-rose-500 duration-100 ease-in-out"
+			
 				>
 					INICIAR SESIÓN
 				</a>
@@ -29,7 +29,7 @@ import MobileLandingMenu from '$lib/components/menu/landing.svelte'
 			<li>
 				<a
 					href="/register"
-					class="px-8 py-4 rounded-lg no-underline text-light-50 font-commissioner bg-rose-500  "
+		
 				>
 					UNETE YA
 				</a>
@@ -39,14 +39,14 @@ import MobileLandingMenu from '$lib/components/menu/landing.svelte'
 	</nav>
 </header>
 <section
-	class="min-h-screen h-full flex flex-col justify-center items-center space-y-5 font-mukta"
+	class="min-h-screen h-full flex flex-col justify-center items-center space-y-5 "
 >
-	<h2 class="text-center xl:text-5xl text-3xl p-2">
-		Imposible olvidar tu <span class="c-rose-500 ">lista de recomendaciones</span>
+	<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+		Imposible olvidar tu <span class="text-rose-500 ">lista de recomendaciones</span>
 		con Codyx
-	</h2>
+	</h1>
 	
-	<a href="/login" class="px-6 py-4 outline-none bg-rose-500 rounded-lg  c-gray-50"
+	<a href="/login" class="px-6 py-4 outline-none bg-rose-500 rounded-lg  text-gray-50"
 		>Comenzar ya</a
 	>
 </section>
