88e19f1a3b8366dc04a5b89a6e8d863834538afa
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-01 14:12:55 -0300

    update img return type

--- src/routes/home/recommendations/create/+page.server.ts
+++ src/routes/home/recommendations/create/+page.server.ts
@@ -1,31 +1,46 @@
 import type { Actions } from './$types'
 import { z } from 'zod'
-import { prisma } from '$lib/server/prisma'
-import { fail, redirect } from '@sveltejs/kit'
-import { env } from '$env/dynamic/private'
-import ImageKit from 'imagekit'
-
-const imageKit = new ImageKit({
-	urlEndpoint: env.IMAGEKIT_ENDPOINT,
-	privateKey: env.IMAGEKIT_PRIVATE_KEY,
-	publicKey: env.IMAGEKIT_PUBLIC_KEY,
-})
+import { fail } from '@sveltejs/kit'
+import { imageKit } from '$lib/server/imagekit'
+import { parseFileToBase64 } from '$lib/server/parse-file'
+import { createRecommendation } from '$lib/server/models/recommendation.server'
 
 const createRecommendationSchema = z.object({
 	name: z.string().min(3, { message: 'Campo requerido' }),
 	note: z.string(),
-	img: z.instanceof(File).refine((f) => f.size < 0, 'Este campo es requerido'),
+	img: z
+		.instanceof(File)
+		.refine((f) => f.size < 0, 'Este campo es requerido')
+		.refine((f) => f.type),
 
 	genre: z.enum(['MOVIE', 'SERIE', 'ANIME', 'MANGA', 'NOVEL', 'OTHER']),
 })
 export const actions: Actions = {
-	'create-recommendation': async ({ request }) => {
+	'create-recommendation': async ({ request, locals }) => {
 		const requestFormData = Object.fromEntries(
 			await request.formData()
 		) as z.infer<typeof createRecommendationSchema>
 		const formData = createRecommendationSchema.safeParse(requestFormData)
 		console.log(formData)
 		if (formData.success) {
+			const session = await locals.getSession()
+			const { genre, img, name, note } = formData.data
+			const parsedImage = await parseFileToBase64(img)
+			const result = await imageKit.upload({
+				file: parsedImage,
+				fileName: formData.data.img.name,
+				tags: ['recommendation'],
+			})
+			const newRecomm = await createRecommendation({
+				name,
+				note,
+				authorEmail: session?.user?.email ?? '',
+				genre,
+				img: { src: result.url, width: result.width, height: result.height },
+			})
+			if (!newRecomm) {
+				return fail(500, { message: 'Error al crear la recomendación' })
+			}
 		} else {
 			const containsErrors = Boolean(
 				formData.error.formErrors.fieldErrors.name ||
@@ -37,7 +52,7 @@ export const actions: Actions = {
 				fields: {
 					name: formData.error.formErrors.fieldErrors.name?.[0],
 					note: formData.error.formErrors.fieldErrors.note?.[0],
-					imgSrc: formData.error.formErrors.fieldErrors.img?.[0],
+					img: formData.error.formErrors.fieldErrors.img?.[0],
 					genre: formData.error.formErrors.fieldErrors.genre?.[0],
 				},
 				externalErrors: undefined,
