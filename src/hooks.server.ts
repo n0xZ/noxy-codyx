fb0bc36f9ee1cb47ecf7b7fca839fbe17b052a2f
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-01 11:53:47 -0300

    chore: setup oauthjs handler

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,16 @@
-import type { Handle } from '@sveltejs/kit'
+import { SvelteKitAuth } from '@auth/sveltekit'
+import GitHub from '@auth/core/providers/github'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private'
+import { prisma } from '$lib/server/prisma'
 
-export const handle = (async ({ event, resolve }) => {
-	event.locals.userId = event.cookies.get('user-session')
-	return resolve(event)
-}) satisfies Handle
+export const handle = SvelteKitAuth({
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		// @ts-ignore
+		GitHub({
+			clientId: GITHUB_CLIENT_ID,
+			clientSecret: GITHUB_CLIENT_SECRET,
+		}),
+	],
+})
