57d50f34967a36d497ddb2890c19bac7357e05da
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-02 14:27:11 -0300

    add new styles on hompage

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
- import type { PageData, PageServerData } from "./$types"
+ import type {PageServerData } from "./$types"
 import RecommendationList from "$lib/components/recommendation/recommendation-list.svelte";
+mport type { Image, Recommendation } from "@prisma/client"
 
 
 export let data :PageServerData
@@ -10,12 +11,13 @@ export let data :PageServerData
 <title>Codyx - Home</title>
 </svelte:head>
 
-<h2 class="mt-4 text-center font-bold text-2xl font-mukta">Mi lista actual de recomendaciones</h2>
+<h2 class="mt-4 text-center font-bold text-2xl ">Mi lista actual de recomendaciones</h2>
 
 {#if data.reccos.length!==0}
 <section class="h-full container mx-auto max-w-4xl   grid xl:grid-cols-3  grid-cols-1 grid-rows-2 gap-5  place-items-center 	mt-9  font-mukta">
-	<RecommendationList props={{reccos:data.reccos}}/>
+
+	<RecommendationList props={{reccos:data.reccos}}  />
 	</section>
 	{:else }
-	<p class="text-center ">Al parecer no tienes recomendados creados hasta ahora. Puedes crearlos haciendo <a class="underline" href="/home/recommendations/create">click aquí</a></p>
+	<article class="h-full grid place-items-center"><p class="text-center ">Al parecer no tienes recomendados creados hasta ahora. Puedes crearlos haciendo <a class="underline" href="/home/recommendations/create">click aquí</a></p></article>
 {/if}
