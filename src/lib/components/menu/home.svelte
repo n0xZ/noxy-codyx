247b111f8b192e10b9e1233f7028e97876f4cdd9
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-02 12:26:24 -0300

    chore: add signOut feat on home menu

--- src/lib/components/menu/home.svelte
+++ src/lib/components/menu/home.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
- import { enhance } from '$app/forms'
+	import { signOut } from '@auth/sveltekit/client'
+	import { env } from '$env/dynamic/public'
+
 	import {
 		Transition,
 		Menu,
@@ -7,7 +9,8 @@
 		MenuItem,
 		MenuItems,
 	} from '@rgossiaux/svelte-headlessui'
-   import MenuIcon from '../icons/menu.svelte'
+	import MenuIcon from '../icons/menu.svelte'
+	const callbackUrl = env.PUBLIC_AUTH_ORIGIN_URL
 </script>
 
 <Menu class="relative xl:hidden lg:hidden inline-block text-left">
@@ -15,11 +18,7 @@
 		<MenuButton
 			class="inline-flex w-full justify-center items-center rounded-md bg-rose-600 bg-opacity-60 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
 		>
-			<MenuIcon
-				
-				menuClass=" h-5 w-5 text-rose-200 hover:text-rose-100"
-		
-			/>
+			<MenuIcon menuClass=" h-5 w-5 text-rose-200 hover:text-rose-100" />
 		</MenuButton>
 	</div>
 	<Transition
@@ -33,7 +32,7 @@
 		<MenuItems
 			class="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
 		>
-			<div class="px-1 py-1 ">
+			<div class="px-1 py-1">
 				<MenuItem>
 					<a
 						href="/home/recommendations/create"
@@ -43,13 +42,12 @@
 					</a>
 				</MenuItem>
 				<MenuItem>
-					<form method="post" use:enhance action="/home?/logout" class="flex flex-row items-center">
-						<button
-							type="submit"
-							class="bg-rose-500 c-white group flex w-full items-center rounded-md px-2 py-2 "
-							>Cerrar sesión</button
-						>
-					</form>
+					<button
+						type="submit"
+						on:click={() => signOut({ redirect: true, callbackUrl })}
+						class="bg-rose-500 c-white group flex w-full items-center rounded-md px-2 py-2"
+						>Cerrar sesión</button
+					>
 				</MenuItem>
 			</div>
 		</MenuItems>
