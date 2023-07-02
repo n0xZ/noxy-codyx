ee4dfef34f1985082d834818f86ce0ea5314081a
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-02 12:22:50 -0300

    chore: add auth handler that redirect if user's not logged in & URL matches home leaf path

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,3 +1,4 @@
+import { redirect, type Handle } from '@sveltejs/kit'
 import { SvelteKitAuth } from '@auth/sveltekit'
 import GitHub from '@auth/core/providers/github'
 import { PrismaAdapter } from '@auth/prisma-adapter'
@@ -7,6 +8,19 @@ import {
 	SESSION_SECRET,
 } from '$env/static/private'
 import { prisma } from '$lib/server/prisma'
+
+const authorization: Handle = async ({ event, resolve }) => {
+	// Protect any routes under /authenticated
+	if (event.url.pathname.startsWith('/home')) {
+		const session = await event.locals.getSession()
+		if (!session) {
+			throw redirect(303, '/auth')
+		}
+	}
+
+	// If the request is still here, just proceed as normally
+	return resolve(event)
+}
 
 export const handle = SvelteKitAuth({
 	adapter: PrismaAdapter(prisma),
@@ -18,4 +32,5 @@ export const handle = SvelteKitAuth({
 		}),
 	],
 	secret: SESSION_SECRET,
+	authorization,
 })
