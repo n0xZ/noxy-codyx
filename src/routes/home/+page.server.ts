83d3d937c7262ab9afef077bfc9be24990a87af3
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-02 14:26:17 -0300

    chore: implement recco. models

--- src/routes/home/+page.server.ts
+++ src/routes/home/+page.server.ts
@@ -1,23 +1,25 @@
-import { redirect, type ServerLoad } from '@sveltejs/kit'
+import type { ServerLoad } from '@sveltejs/kit'
 import { prisma } from '../../lib/server/prisma'
 import type { Actions } from './$types'
+import {
+	deleteRecommendation,
+	recommendationsByAuthor,
+} from '$lib/server/models/recommendation.server'
 
 export const load: ServerLoad = async ({ locals }) => {
 	const session = await locals.getSession()
-	if (!session) throw redirect(302, '/')
-	const userReccomendations = await prisma.recommendation.findMany({
-		where: { author: { email: session.user?.email ?? '' } },
-	})
+
+	const userReccomendations = await recommendationsByAuthor(
+		session?.user?.email ?? ''
+	)
+
 	return { reccos: userReccomendations }
 }
 export const actions: Actions = {
 	'delete-recommendation': async ({ request }) => {
 		const formData = await request.formData()
-		const recommId = formData.get('id') as string
+		const id = formData.get('id') as string
 
-		await prisma.recommendation.delete({
-			where: { id: recommId },
-		})
-		throw redirect(302, '/home')
+		await deleteRecommendation(id)
 	},
 }
