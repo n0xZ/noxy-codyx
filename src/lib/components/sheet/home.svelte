b09a50187c508d64b6ef54aeffbd0618b38bf4ad
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-03 18:10:15 -0300

    chore: setup home sheet comp.

---
+++ src/lib/components/sheet/home.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { Avatar, AvatarFallback, AvatarImage } from '$lib/ui/avatar'
+	import Button from '$lib/ui/button.svelte'
+	import {
+		Sheet,
+		SheetContent,
+		SheetDescription,
+		SheetFooter,
+		SheetHeader,
+		SheetTitle,
+		SheetTrigger,
+	} from '$lib/ui/sheet'
+	import type { Session } from '@auth/core/types'
+	export let userMetadata: Session | null
+	const username = userMetadata?.user?.name
+	const fallbackUsername = username?.slice(0, 2)
+</script>
+
+<Sheet>
+	<SheetTrigger>
+		<Avatar>
+			<AvatarImage
+				src={userMetadata?.user?.image ?? ''}
+				alt={`Imagen de ${userMetadata?.user?.name}`}
+			/>
+			<AvatarFallback>{fallbackUsername}</AvatarFallback>
+		</Avatar>
+	</SheetTrigger>
+	<SheetContent position="left" size="sm">
+		<SheetHeader>
+			<SheetTitle>Perfil</SheetTitle>
+			<SheetDescription>
+				Bienvenid@, {username}!
+			</SheetDescription>
+		</SheetHeader>
+		<aside class="grid gap-3 py-3 opacity-60">
+			<a href="/home/recommendations/create" class="hover:underline"
+				>Crear nueva recomendación</a
+			>
+		</aside>
+		<SheetFooter>
+			<Button>Cerrar sesión</Button>
+		</SheetFooter>
+	</SheetContent>
+</Sheet>
