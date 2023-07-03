6909e07454e86744263c226a8f00e208508578f5
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-03 09:13:40 -0300

    chore: set signOutUser func that signOut's user without client-side refresh

--- src/routes/home/+layout.svelte
+++ src/routes/home/+layout.svelte
@@ -1,30 +1,27 @@
 <script lang="ts">
-		import { signOut } from '@auth/sveltekit/client';
+	import { signOut } from '@auth/sveltekit/client'
 	import { env } from '$env/dynamic/public';
-
-
-	import MobileHomeMenu from "$lib/components/menu/home.svelte"
 	const callbackUrl = env.PUBLIC_AUTH_ORIGIN_URL;
+	import MobileHomeMenu from '$lib/components/menu/home.svelte'
+
+	const signOutUser = () => signOut({redirect:false,callbackUrl})
 </script>
 
-<header class="p-4 ">
+<header class="p-5 w-full">
 	<nav
-		class="flex flex-row items-center justify-between container mx-auto max-w-4xl xl:text-lg text-base  "
+		class="flex flex-row items-center justify-between container w-full mx-auto max-w-4xl text-base"
 	>
 		<h1>
-			<a href="/home" class="no-underline text-rose-500  font-semibold"> Codyx </a>
+			<a href="/home" class="no-underline text-rose-500 font-semibold"> Codyx </a>
 		</h1>
-		<ul class="xl:flex hidden flex-row items-center space-x-4  ">
+		<ul class="xl:flex hidden flex-row items-center space-x-4 opacity-70">
 			<a href="/home/recommendations/create">Crear nueva recomendación</a>
-			
-				<button
-					type="submit"
-					class="p-3 rounded-lg bg-rose-500 text-gray-50 font-semibold "
-					on:click={() => signOut({ redirect: true, callbackUrl })}
 
-					>Cerrar sesión</button
-				>
-
+			<button
+				type="button"
+				class="rounded-lg p-3 bg-rose-500 text-gray-50 font-semibold"
+				on:click={signOutUser}>Cerrar sesión</button
+			>
 		</ul>
 		<MobileHomeMenu />
 	</nav>
