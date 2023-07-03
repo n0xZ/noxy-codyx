ea871340cbec3990a807f8d9ee1dbc475ea234f7
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-03 11:02:54 -0300

    chore: throw error if user's does not exist & its content is private

--- src/lib/server/models/user.server.ts
+++ src/lib/server/models/user.server.ts
@@ -1,10 +1,17 @@
 import { prisma } from '../prisma'
-
+import { error } from '@sveltejs/kit'
 export const userById = async (id?: string) => {
-	const user = await prisma.user.findUniqueOrThrow({
+	const user = await prisma.user.findUnique({
 		where: { id },
-		select: { recommendations: true, name: true, image: true },
+		select: {
+			recommendations: true,
+			name: true,
+			image: true,
+			isContentPublic: true,
+		},
 	})
-
+	if (!user) throw error(404, 'Usuario no encontrado')
+	if (!user.isContentPublic)
+		throw error(403, 'Las recomendaciones de este usuario son privadas.')
 	return user
 }
