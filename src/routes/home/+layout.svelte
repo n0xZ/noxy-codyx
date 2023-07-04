4117e3bb14e9f1d6e2b02e3b692c0a769a0f91dd
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-04 00:23:45 -0300

    chore: add theme switcher on home layout

--- src/routes/home/+layout.svelte
+++ src/routes/home/+layout.svelte
@@ -1,21 +1,26 @@
 <script lang="ts">
-	import { signOut } from '@auth/sveltekit/client'
-	import { env } from '$env/dynamic/public'
-
 	import HomeSheet from '$lib/components/sheet/home.svelte'
+	import ThemeSwitcher from '$lib/components/theme/theme-switcher.svelte'
 	import type { LayoutServerData } from './$types'
 
-	const callbackUrl = env.PUBLIC_AUTH_ORIGIN_URL
-	const signOutUser = () => signOut({ redirect: false, callbackUrl })
 	export let data: LayoutServerData
 </script>
 
-<header class="p-5 w-full border-b-2 border-primary/5 sticky top-0 bg-white">
+<header
+	class="p-5 w-full border-b-2 border-primary/5 sticky top-0 bg-background"
+>
 	<nav
 		class="flex flex-row items-center justify-between container w-full mx-auto max-w-4xl text-base"
 	>
-		<a href="/home" class="hover:underline" data-sveltekit-preload-data="hover">Home</a>
-		<HomeSheet userMetadata={data.session} />
+		<a href="/home" class="hover:underline" data-sveltekit-preload-data="hover"
+			>Home</a
+		>
+		<ul class="flex flex-row items-center space-x-5">
+			<li><ThemeSwitcher /></li>
+			<li>
+				<HomeSheet userMetadata={data.session} />
+			</li>
+		</ul>
 	</nav>
 </header>
 <div class="h-full mt-4"><slot /></div>
