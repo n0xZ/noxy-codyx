92f443b8b64ab453b5ba7f1a48eb18e4c3ccaa1e
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-04 18:13:23 -0300

    chore: add svelte toast & alert dialog on homepage

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,22 +1,63 @@
 <script lang="ts">
- import type {PageServerData } from "./$types"
-import RecommendationList from "$lib/components/recommendation/recommendation-list.svelte";
+	import { browser } from '$app/environment'
+	import { SvelteToast } from '@zerodevx/svelte-toast';
+	import type { ActionData, PageServerData } from './$types'
+	import Button from '$lib/ui/button.svelte'
+	import {
+		AlertDialog,
+		AlertDialogTrigger,
+	} from '$lib/ui/alert-dialog'
+	import RecommendationList from '$lib/components/recommendation/recommendation-list.svelte'
+	import UpdateShareForm from '$lib/components/alert-dialog/update-share-form.svelte'
+	import DisplayShareLink from '$lib/components/alert-dialog/display-share-link.svelte'
 
-
-export let data :PageServerData
-
+	export let data: PageServerData
+	export let form: ActionData
+	$: {
+		if (form?.isSucess) location.reload()
+	}
 </script>
+
 <svelte:head>
-<title>Codyx - Home</title>
+	<title>Codyx - Home</title>
 </svelte:head>
 
-<h2 class="mt-4 text-center font-bold text-2xl ">Mi lista actual de recomendaciones</h2>
-
-{#if data.reccos.length!==0}
-<section class="h-full container mx-auto max-w-4xl   grid xl:grid-cols-3  grid-cols-1 grid-rows-2 gap-5  place-items-center 	mt-9  font-mukta">
-
-	<RecommendationList props={{reccos:data.reccos}}  />
+<aside class="p-5 border-b-2 border-primary/10">
+	<div
+		class="container mx-auto max-w-4xl flex items-center justify-end flex-row"
+	>
+		<AlertDialog>
+			<AlertDialogTrigger>
+				<Button variant="outline">Compartir lista 📒</Button>
+			</AlertDialogTrigger>
+			{#if data.userMetadata.isContentPublic}
+				<UpdateShareForm />
+			{:else}
+				<DisplayShareLink id={data.userMetadata.id} />
+			{/if}
+		</AlertDialog>
+	</div>
+</aside>
+<h2 class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0 text-center mt-2">
+	Mi lista actual de recomendaciones
+</h2>
+{#if data.reccos.length !== 0}
+	<section
+		class="h-full container mx-auto max-w-4xl grid xl:grid-cols-3 grid-cols-1 grid-rows-2 gap-5 place-items-center mt-9 "
+	>
+		<RecommendationList props={{ reccos: data.reccos }} />
 	</section>
-	{:else }
-	<article class="h-full grid place-items-center"><p class="text-center ">Al parecer no tienes recomendados creados hasta ahora. Puedes crearlos haciendo <a class="underline" href="/home/recommendations/create">click aquí</a></p></article>
+{:else}
+	<article class="h-full grid place-items-center">
+		<p class="text-center">
+			Al parecer no tienes recomendados creados hasta ahora. Puedes crearlos
+			haciendo <a class="underline" href="/home/recommendations/create"
+				>click aquí</a
+			>
+		</p>
+	</article>
+{/if}
+
+{#if browser}
+	<SvelteToast />
 {/if}
