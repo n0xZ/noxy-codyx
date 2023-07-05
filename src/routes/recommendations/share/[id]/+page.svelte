60e0b69dd5a3a937414ddcb911238f0564ca45fb
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-05 18:20:21 -0300

    chore: add title on share page

--- src/routes/recommendations/share/[id]/+page.svelte
+++ src/routes/recommendations/share/[id]/+page.svelte
@@ -3,19 +3,23 @@
 	import { Avatar, AvatarFallback, AvatarImage } from '$lib/ui/avatar'
 	import RecommendationList from '$lib/components/recommendation/recommendation-list.svelte'
 	export let data: PageServerData
-const fallbackUsername  = data.user.name?.slice(0, 2)
+	const fallbackUsername = data.user.name?.slice(0, 2)
 </script>
 
 <main class="h-full container mx-auto max-w-4xl">
-<aside class="p-5  flex flex-row items-center justify-center space-x-2">
-	<Avatar>
-		<AvatarImage
-			src={data.user.image ?? ""}
-			alt={`Imagen de ${data.user.name}`}
-		/>
-		<AvatarFallback>{fallbackUsername}</AvatarFallback>
-	</Avatar>
-  <p  class="font-medium">{data.user.name}</p >
-</aside>
-	<RecommendationList props={{ reccos: data.user.recommendations }} />
+<h2 class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0 text-center"
+>Lista de recomendaciones</h2>
+	<aside class="p-5 flex flex-row items-center justify-center space-x-2 mb-6">
+		<Avatar>
+			<AvatarImage
+				src={data.user.image ?? ''}
+				alt={`Imagen de ${data.user.name}`}
+			/>
+			<AvatarFallback>{fallbackUsername}</AvatarFallback>
+		</Avatar>
+		<p class="font-medium">Creado por {data.user.name}</p>
+	</aside>
+	<section class="w-full grid">
+		<RecommendationList props={{ reccos: data.user.recommendations }} />
+	</section>
 </main>
