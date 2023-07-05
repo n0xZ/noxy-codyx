aa6620f7871cfb6861814ff4f0367dad8d1ae0f8
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-05 14:24:20 -0300

    chore: setup fallback font on tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,7 +13,10 @@ module.exports = {
 			},
 		},
 		fontFamily: {
-			inter: 'Inter',
+			inter: [
+				'Inter',
+				'Inter,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji',
+			],
 		},
 		extend: {
 			colors: {
