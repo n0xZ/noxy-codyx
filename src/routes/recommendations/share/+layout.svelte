0d946de5946449f5901358185cffe65c9cd8dc77
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-05 11:08:19 -0300

    chore: add layout on share path to display header anchors based on user session status

---
+++ src/routes/recommendations/share/+layout.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import type { LayoutServerData } from './$types'
+
+	export let data: LayoutServerData
+</script>
+
+<header class="p-6">
+	<nav
+		class="flex flex-row items-center justify-between container mx-auto max-w-4xl xl:text-lg text-base"
+	>
+		<h1>
+			<a
+				href="/"
+				class="font-bold"
+				>Codyx</a
+			>
+		</h1>
+		<ul class="flex flex-row items-center space-x-4 opacity-90">
+			<li>
+				{#if !data.isAuth}
+					<a href="/login" class="hover:underline"  data-sveltekit-preload-data="hover"> Iniciar sesión </a>
+				{:else}
+					<a href="/home"  class="hover:underline" data-sveltekit-preload-data="hover"> Volver al Home </a>
+				{/if}
+			</li>
+		</ul>
+	</nav>
+</header>
+
+<slot/>
