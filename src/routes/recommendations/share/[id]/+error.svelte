c6944f74f77b809cb36309cd97433b7acfd616ec
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-06 11:27:14 -0300

    chore: setup error pages

--- src/routes/recommendations/share/[id]/+error.svelte
+++ src/routes/recommendations/share/[id]/+error.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
-import {page} from '$app/stores' 
+	import { page } from '$app/stores'
+	import Button from '$lib/ui/button.svelte'
 
+	const reloadPage = () => location.reload()
 </script>
 
-<h2>Ha ocurrido un error. No te preocupes, ya estoy trabajando en ello 🙂 {$page.error?.message}</h2>
+<section class="h-screen flex flex-col justify-center items-center space-y-2">
+	<h2
+		class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+	>
+		Ups! Al parecer ocurrió un error. No te preocupes,
+		ya estoy trabajando en ello. 😀
+	</h2>
+	<p>{$page.error?.message}</p>
+	<Button on:click={reloadPage}>Refrescar página</Button>
+</section>
