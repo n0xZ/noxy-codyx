7dc6e8abea2cee208e3451c671a12be13da0d5a8
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-08-03 17:40:33 -0300

    fix styles on status select element

--- src/routes/home/recommendations/edit/[id]/+page.svelte
+++ src/routes/home/recommendations/edit/[id]/+page.svelte
@@ -116,7 +116,7 @@ class="flex h-10 w-full rounded-md border border-input bg-transparent px-3 py-2 
 					name="status"
 					value={data.form?.status}
 					disabled={loading}
-			class="flex h-10 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+			class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
 				>
 					{#each actualStatus as status}
 						<option value={status}>
