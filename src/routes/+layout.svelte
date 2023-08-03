76ca12808b1ac641a5bd2b21396a7226711ab841
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-08-03 16:54:45 -0300

    chore: add progress bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
+import { ProgressBar } from '@prgm/sveltekit-progress-bar'
   import "../app.postcss";
 
 </script>
-
+<ProgressBar class="text-rose-500"/>
 <main class=" bg-background text-primary h-full w-full min-h-screen font-inter"><slot /></main>
